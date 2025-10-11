53a824a5dd88841a69e3ab6151202e702b035325
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-10-11 09:16:23 +0300

    make state global

--- shopee/src/context/AppContext.tsx
+++ shopee/src/context/AppContext.tsx
@@ -9,12 +9,20 @@ import {
   getProfileFromLocalStorage,
 } from "../untils/auth.api";
 import type { User } from "../types/user.type";
-
+interface PurchaseItem {
+  _id: string;
+  product: any;
+  buy_count: number;
+  isChecked: boolean;
+  isDisabled: boolean;
+}
 type AppContextType = {
   isLogin: boolean;
   setIsLogin: (isLogin: boolean) => void;
   profile: User | null;
   setProfile: Dispatch<SetStateAction<User | null>>;
+  extendedPurchaseInCart: PurchaseItem[];
+  setExtendedPurchaseInCart: Dispatch<SetStateAction<PurchaseItem[]>>;
 };
 
 const initialAppContext: AppContextType = {
@@ -22,7 +30,10 @@ const initialAppContext: AppContextType = {
   setIsLogin: () => null,
   setProfile: () => null,
   profile: getProfileFromLocalStorage() as User | null,
+  extendedPurchaseInCart: [],
+  setExtendedPurchaseInCart: () => null,
 };
+
 export const AppContext = createContext(initialAppContext);
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
@@ -30,8 +41,20 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [profile, setProfile] = useState<User | null>(
     initialAppContext.profile
   );
+  const [extendedPurchaseInCart, setExtendedPurchaseInCart] = useState<
+    PurchaseItem[]
+  >([]);
   return (
-    <AppContext.Provider value={{ isLogin, setIsLogin, profile, setProfile }}>
+    <AppContext.Provider
+      value={{
+        isLogin,
+        setIsLogin,
+        profile,
+        setProfile,
+        extendedPurchaseInCart,
+        setExtendedPurchaseInCart,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
